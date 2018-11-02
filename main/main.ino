a992ea7b3ece540893c45ba393a770ba14ceb296
Devi Acharya <dacharya64@gmail.com>
2018-11-02 11:59:54 -0700

    updating comments with version

--- main/main.ino
+++ main/main.ino
@@ -2,8 +2,8 @@
  * This is the main file for DANM 219 final project 
  * It uses an input heartbeat sensor to control audio output and LED light strands. 
  * 
- * NOTE: To use, please make sure PuseSensor Playground is installed. 
- * Sketch -> Include Library -> Manage Libraries -> search for "PulseSensor Playground" and install version 
+ * NOTE: To use, please make sure PulseSensor Playground is installed. 
+ * Sketch -> Include Library -> Manage Libraries -> search for "PulseSensor Playground" and install version 1.4.11 (currently the latest version)
  */
  
 #define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
