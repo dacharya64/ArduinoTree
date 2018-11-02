ac1ba8fe8ed494d62ee3859f88d5d936b0a49093
Devi Acharya <dacharya64@gmail.com>
2018-11-02 11:32:42 -0700

    Adding main file for project

---
+++ main/main.ino
@@ -0,0 +1,16 @@
+/*
+ * This is the main file for DANM 219 final project 
+ * It uses an input heartbeat sensor to control audio output and LED light strands. 
+ * 
+ * NOTE: To use, please 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
