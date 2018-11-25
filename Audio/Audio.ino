a8ef99efc257ea5a8ce4ad75fb37c57389b083b9
Devi Acharya <dacharya64@gmail.com>
2018-11-24 22:28:02 -0800

    Some test audio code

---
+++ Audio/Audio.ino
@@ -0,0 +1,157 @@
+/*
+   This is the main file for DANM 219 final project
+   It uses an input heartbeat sensor to control audio output and LED light strands.
+
+   NOTE: To use, please make sure PulseSensor Playground and WaveHC libraries are installed.
+   Sketch -> Include Library -> Manage Libraries -> search for "PulseSensor Playground" and install version 1.4.11 (currently the latest version) and "WaveHC" (version 1.0)
+*/
+
+//#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+//#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+#include "WaveUtil.h"
+#include "WaveHC.h"
+#include <FatReader.h>
+#include <SdReader.h>
+#include <avr/pgmspace.h>
+
+SdReader card;    // This object holds the information for the card
+FatVolume vol;    // This holds the information for the partition on the card
+FatReader root;   // This holds the information for the filesystem on the card
+FatReader f;      // This holds the information for the file we're playing
+ 
+WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
+
+//const int PULSE_INPUT = A0;
+//const int THRESHOLD = 550;   // Adjust this number to avoid noise when idle
+
+/*
+   All the PulseSensor Playground functions.
+*/
+//PulseSensorPlayground pulseSensor;
+
+void setup() {
+  
+
+  
+  /*
+     Use 115200 baud because that's what the Processing Sketch expects to read,
+     and because that speed provides about 11 bytes per millisecond.
+
+     If we used a slower baud rate, we'd likely write bytes faster than
+     they can be transmitted, which would mess up the timing
+     of readSensor() calls, which would make the pulse measurement
+     not work properly.
+  */
+  Serial.begin(115200);
+
+  putstring("Free RAM: ");       // This can help with debugging, running out of RAM is bad
+  Serial.println(freeRam());      // if this is under 150 bytes it may spell trouble!
+  
+  pinMode(13, OUTPUT);
+ 
+  //  if (!card.init(true)) { //play with 4 MHz spi if 8MHz isn't working for you
+  if (!card.init()) {         //play with 8 MHz spi (default faster!)  
+    putstring_nl("Card init. failed!");  // Something went wrong, lets print out why
+    sdErrorCheck();
+    while(1);                            // then 'halt' - do nothing!
+  }
+  
+  // enable optimize read - some cards may timeout. Disable if you're having problems
+  card.partialBlockRead(true);
+  
+// Now we will look for a FAT partition!
+  uint8_t part;
+  for (part = 0; part < 5; part++) {     // we have up to 5 slots to look in
+    if (vol.init(card, part)) 
+      break;                             // we found one, lets bail
+  }
+  if (part == 5) {                       // if we ended up not finding one  :(
+    putstring_nl("No valid FAT partition!");
+    sdErrorCheck();      // Something went wrong, lets print out why
+    while(1);                            // then 'halt' - do nothing!
+  }
+  
+  // Lets tell the user about what we found
+  putstring("Using partition ");
+  Serial.print(part, DEC);
+  putstring(", type is FAT");
+  Serial.println(vol.fatType(),DEC);     // FAT16 or FAT32?
+  
+  // Try to open the root directory
+  if (!root.openRoot(vol)) {
+    putstring_nl("Can't open root dir!"); // Something went wrong,
+    while(1);                             // then 'halt' - do nothing!
+  }
+  
+  // Whew! We got past the tough parts.
+  putstring_nl("Ready!");
+  putstring_nl("Setting up...");
+  playcomplete("SO.WAV");
+  putstring_nl("Done setting up");
+}
+
+void sdErrorCheck(void)
+{
+  if (!card.errorCode()) return;
+  putstring("\n\rSD I/O error: ");
+  Serial.print(card.errorCode(), HEX);
+  putstring(", ");
+  Serial.println(card.errorData(), HEX);
+  while(1);
+}
+
+
+// Plays a full file from beginning to end with no pause.
+void playcomplete(char *name) {
+  // call our helper to find and play this name
+  putstring_nl("Playing...");
+  playfile(name);
+  while (wave.isplaying) {
+  // do nothing while its playing
+  }
+  // now its done playing
+  putstring_nl("Done");
+}
+ 
+void playfile(char *name) {
+  // see if the wave object is currently doing something
+  if (wave.isplaying) {// already playing something, so stop it!
+    wave.stop(); // stop it
+    putstring_nl("Stopped playing");
+  }
+  // look in the root directory and open the file
+  if (!f.open(root, name)) {
+    putstring("Couldn't open file "); Serial.print(name); return;
+  }
+  // OK read the file and turn it into a wave object
+  if (!wave.create(f)) {
+    putstring_nl("Not a valid WAV"); return;
+  }
+  wave.create(f);
+  
+  // ok time to play! start playback
+  wave.play();
+  
+}
+
+// this handy function will return the number of bytes currently free in RAM, great for debugging!   
+int freeRam(void)
+{
+  extern int  __bss_end; 
+  extern int  *__brkval; 
+  int free_memory; 
+  if((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__bss_end); 
+  }
+  else {
+    free_memory = ((int)&free_memory) - ((int)__brkval); 
+  }
+  return free_memory; 
+} 
+
+
+
+void loop() {
+  playcomplete("SO.WAV");
+  delay(3000);
+}
